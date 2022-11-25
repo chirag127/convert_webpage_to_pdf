8914fb20ef80c69838ee885941ed0f5c20ba1870
Chirag <76880977+chirag127@users.noreply.github.com>
2022-11-25 12:41:44 +0000

    "-y" ,

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,19 @@ import subprocess
 try:
     # sudo apt-get update
     # sudo apt-get install wkhtmltopdf
-    subprocess.call(['wkhtmltopdf', '--version'])
+    subprocess.call(["wkhtmltopdf", "--version"])
     wkhtmltopdf_installed = True
     print("wkhtmltopdf installed")
 except:
     wkhtmltopdf_installed = False
     print("wkhtmltopdf not installed")
 
-    # install wkhtmltopdf
-    subprocess.call(['sudo', 'apt-get', 'update'])
-    subprocess.call(['sudo', 'apt-get', 'install', 'wkhtmltopdf'])
 
+if not wkhtmltopdf_installed:
+
+    # install wkhtmltopdf
+    subprocess.call(["sudo", "apt-get", "-y", "update"])
+    subprocess.call(["sudo", "apt-get", "install", "-y", "wkhtmltopdf"])
 
 
 # this is an python app to convert the webpages to pdf
@@ -32,13 +34,19 @@ def main():
     name = st.text_input("Enter the name")
     if st.button("Convert"):
         st.text("Converting...")
-        pdfkit.from_url(url, name+".pdf")
+        pdfkit.from_url(url, name + ".pdf")
         st.success("Done")
 
-        with open(name+".pdf", "rb") as pdf_file:
+        with open(name + ".pdf", "rb") as pdf_file:
             PDFbyte = pdf_file.read()
 
-        st.download_button(label="download", data=PDFbyte, file_name=name+".pdf", mime="application/pdf")
+        st.download_button(
+            label="download",
+            data=PDFbyte,
+            file_name=name + ".pdf",
+            mime="application/pdf",
+        )
+
 
 if __name__ == "__main__":
     main()
