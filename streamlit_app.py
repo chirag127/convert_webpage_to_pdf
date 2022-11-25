bec5515a1f1f54c358e7f4c9a6354985b45bc526
Chirag <76880977+chirag127@users.noreply.github.com>
2022-11-25 12:23:04 +0000

    streamlit_app

---
+++ streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pdfkit
+
+# this is an python app to convert the webpages to pdf
+
+# this is the main function
+def main():
+    st.title("Webpage to PDF Converter")
+    st.text("This app converts the webpage to pdf")
+    st.text("Enter the url of the webpage")
+    url = st.text_input("Enter the url")
+    st.text("Enter the name of the pdf file")
+    name = st.text_input("Enter the name")
+    if st.button("Convert"):
+        st.text("Converting...")
+        pdfkit.from_url(url, name+".pdf")
+        st.success("Done")
+
+        with open(name+".pdf", "rb") as pdf_file:
+            PDFbyte = pdf_file.read()
+
+        st.download_button(label="download", data=PDFbyte, file_name=name+".pdf", mime="application/pdf")
+
+if __name__ == "__main__":
+    main()
